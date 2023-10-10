b619261360e9d243de8485bfd67525ac8f0fd0cb
Igor Nascimento <trabalho.computador.sjn@gmail.com>
2023-10-10 10:29:50 -0300

    criação do get e do insert

--- CRUD/trilhas/models.py
+++ CRUD/trilhas/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Aluno(models.Model):
-    nome = models.CharField('Nome', max_length=100),
-    idade = models.IntegerField("Idade"),
+    nome = models.CharField(max_length=100)
+    idade = models.IntegerField()
     
--- CRUD/trilhas/views.py
+++ CRUD/trilhas/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from .models import Aluno
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    alunos = Aluno.objects.all()
+    return render(request, "index.html", {'alunos':alunos})
+
+def create(request):
+    Aluno.objects.create(nome=request.POST['nome'], idade=int(request.POST['idade']))
+    alunos = Aluno.objects.all()
+    return render(request, "index.html", {'alunos':alunos})
